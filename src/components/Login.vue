4a67a376166bdb805bd5e970677f48808f88b60e
yino <yinoes@126.com>
2025-06-21 17:33:31 +0800

    feat 表单界面完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,57 +1,66 @@
 <template>
   <div class="login-bg">
     <div class="login-container">
-      <div class="login-box">
+      <div class="login-content">
         <!-- 左上装饰 -->
         <div class="deco deco-tl">
           <div class="deco-rect deco-rect-solid"></div>
           <div class="deco-rect deco-rect-outline"></div>
         </div>
-        <div>
-          <h1 class="login-title">Mandlab</h1>
-          <el-form>
-            <el-form-item>
-              <label class="input-label">邀请码</label>
-              <el-input v-model="inviteCode" placeholder="请输入邀请码" />
-            </el-form-item>
-            <div>
-              <!-- <span class="invite-tip">
+        <div class="login-box">
+          <div>
+            <h1 class="login-title">Mandlab</h1>
+            <el-form>
+              <el-form-item>
+                <label class="input-label">邀请码</label>
+                <el-input v-model="inviteCode" placeholder="请输入邀请码" />
+              </el-form-item>
+              <div>
+                <!-- <span class="invite-tip">
                 没有邀请码？<a class="invite-link" href="#">联系我们</a>
               </span> -->
-            </div>
-            <el-form-item>
-              <label class="input-label">手机号码</label>
-              <el-input v-model="phone" placeholder="请输入手机号码" />
-            </el-form-item>
-            <div>
-              <p class="input-label">验证码</p>
-            </div>
-            <el-form-item class="captcha-row-form">
-              <el-col :span="16">
-                <el-input v-model="captcha" placeholder="请输入验证码" />
-              </el-col>
-              <el-col :span="2"></el-col>
-              <el-col :span="6">
-                <el-button class="captcha-btn" type="primary" plain
-                  >发送验证码</el-button
+              </div>
+              <el-form-item>
+                <label class="input-label">手机号码</label>
+                <el-input v-model="phone" placeholder="请输入手机号码" />
+              </el-form-item>
+              <!-- 验证码相关组 -->
+              <div>
+                <p class="input-label">验证码</p>
+              </div>
+              <el-form-item class="captcha-row-form">
+                <el-row :gutter="28" class="captcha-row-form-item">
+                  <el-col :span="19">
+                    <el-input v-model="captcha" placeholder="请输入验证码" />
+                  </el-col>
+                  <el-col :span="2">
+                    <el-button class="captcha-btn" type="primary" plain
+                      >发送验证码</el-button
+                    >
+                  </el-col>
+                </el-row>
+              </el-form-item>
+              <!-- 协议相关 -->
+              <el-form-item class="agreement-group">
+                <el-col :span="3"></el-col>
+                <el-col :span="18">
+                  <el-checkbox v-model="agreed">
+                    <span class="agreement-text">
+                      已阅读并同意Mandlab
+                      <a class="agreement-link" href="#">使用协议</a>
+                      和
+                      <a class="agreement-link" href="#">隐私政策</a>
+                    </span>
+                  </el-checkbox>
+                </el-col>
+              </el-form-item>
+              <el-form-item>
+                <el-button class="login-btn" type="success" round block
+                  >登录</el-button
                 >
-              </el-col>
-            </el-form-item>
-            <el-form-item>
-              <el-checkbox v-model="agreed" />
-              <span class="agreement-text">
-                已阅读并同意Mandlab
-                <a class="agreement-link" href="#">使用协议</a>
-                和
-                <a class="agreement-link" href="#">隐私政策</a>
-              </span>
-            </el-form-item>
-            <el-form-item>
-              <el-button class="login-btn" type="success" round block
-                >登录</el-button
-              >
-            </el-form-item>
-          </el-form>
+              </el-form-item>
+            </el-form>
+          </div>
         </div>
         <!-- 右下装饰放到 login-box 内部 -->
         <div class="deco deco-br">
@@ -72,14 +81,16 @@ const agreed = ref(false);
 
 <style scoped>
 .login-bg {
-  min-height: 100vh;
+  width: 100%;
+  height: 100%;
+  /* min-height: 100vh; */
   background: linear-gradient(180deg, #e3f5e9 0%, #fff 60%);
   position: relative;
   overflow: hidden;
   font-size: 1rem;
 }
 .login-container {
-  min-height: 100vh;
+  min-height: 60vh;
   display: flex;
   align-items: flex-start;
   justify-content: center;
@@ -87,6 +98,10 @@ const agreed = ref(false);
   position: relative;
   padding-top: 18vh;
   padding-bottom: 2vh;
+}
+/* 为了配合装饰物 */
+.login-content {
+  position: relative;
 }
 .login-box {
   background: #fff;
@@ -102,6 +117,7 @@ const agreed = ref(false);
   margin-left: 2vw;
   margin-right: 2vw;
   z-index: 1;
+  opacity: 0.8;
 }
 .deco {
   position: absolute;
@@ -110,17 +126,21 @@ const agreed = ref(false);
 }
 .deco-tl {
   top: -4vw;
-  left: -4vw;
+  left: -2vw;
   width: 6vw;
   height: 5vw;
 }
+/* 右下 */
 .deco-br {
   position: absolute;
-  right: 1rem;
-  bottom: 0.5rem;
+  right: 3vw;
+  bottom: 1vw;
   width: 3rem;
   height: 3rem;
   z-index: -1;
+}
+.deco-br .deco-rect-solid {
+  border: 1px solid #969696;
 }
 .deco-rect {
   position: absolute;
@@ -144,11 +164,7 @@ const agreed = ref(false);
   background: #fff;
   z-index: 1;
 }
-.deco-br .deco-rect-solid {
-  top: 0.7vw;
-  right: -4vw;
-  bottom: -2vw;
-}
+/* login form */
 .login-title {
   color: #3dbc79;
   font-size: 2.1rem;
@@ -187,6 +203,10 @@ const agreed = ref(false);
   color: #0d7dd9;
   border-color: #0d7dd9;
 }
+/*  协议弹窗相关 */
+.agreement-group {
+  margin-top: 2rem;
+}
 .agreement-text {
   font-size: 0.8rem;
   color: #222;
@@ -221,6 +241,12 @@ const agreed = ref(false);
 .el-form-item {
   margin-bottom: 0.5rem;
 }
+/* 验证码模块 */
+.captcha-row-form .captcha-row-form-item  {
+  width: 100%;
+}
+
+/* 响应式 */
 @media (max-width: 900px) {
   .login-box {
     min-width: 60vw;
@@ -233,8 +259,7 @@ const agreed = ref(false);
     padding-top: 4vh;
     padding-bottom: 2vh;
   }
-  .deco-tl,
-  .deco-br {
+  .deco-tl {
     width: 3vw;
     height: 2vw;
     top: -1vw;
