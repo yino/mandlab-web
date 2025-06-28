7dec5d5ba579acbaf36dfd72c9ea05e8043f2fe5
yino <yinoes@126.com>
2025-06-28 22:27:18 +0800

    feat: 删除默认手机号D

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -107,8 +107,8 @@ import AgreementDialog from "../components/agreement.vue";
 
 const inviteCode = ref("");
 const registerOk = ref(true);
-const phone = ref("15829090357");
-const captcha = ref("123456");
+const phone = ref("");
+const captcha = ref("");
 const agreed = ref(false);
 const dialogVisible = ref(false);
 const loginBtnTxt = ref("登录");
